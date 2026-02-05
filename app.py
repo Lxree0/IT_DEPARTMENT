220f3527eb450644cb9a3e579569cf81666e28f6
Lxree <154974082+Lxree0@users.noreply.github.com>
2026-02-05 20:51:10 +0000

    finito

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
-def carica_comp():
-    try:
-        with open('competizioni.json', 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return []
 
 @app.route("/")
 def home():
@@ -18,19 +12,11 @@ def progetti():
 
 @app.route("/competizioni")
 def competizioni():
-    competizioni = carica_comp
-    return render_template("competizioni.html", competizioni=competizioni)
+    return render_template("competizioni.html")
 
-@app.route('/competizione/<int:comp_id>')
-def dettaglio(comp_id):
-    competizioni = carica_dati()
-    gara = next((item for item in competizioni if item["id"] == comp_id), None)
-    
-    if gara is None:
-        abort(404)
-        
-    return render_template('dettaglio_comp.html', gara=gara)
-
+@app.route("/contatti")
+def contatti():
+    return render_template("contatti.html")
 
 if (__name__ == "__main__"):
     app.run(debug=True)
