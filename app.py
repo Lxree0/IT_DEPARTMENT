ed42f36a59286331c5cec0fac3a55e6aa2734cd5
SimoneGiove <simonegiove316@gmail.com>
2026-02-05 20:45:53 +0100

    aggiunte competizioni

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
+def carica_comp():
+    try:
+        with open('competizioni.json', 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
 
 @app.route("/")
 def home():
@@ -12,7 +18,18 @@ def progetti():
 
 @app.route("/competizioni")
 def competizioni():
-    return render_template("competizioni.html")
+    competizioni = carica_comp
+    return render_template("competizioni.html", competizioni=competizioni)
+
+@app.route('/competizione/<int:comp_id>')
+def dettaglio(comp_id):
+    competizioni = carica_dati()
+    gara = next((item for item in competizioni if item["id"] == comp_id), None)
+    
+    if gara is None:
+        abort(404)
+        
+    return render_template('dettaglio_comp.html', gara=gara)
 
 
 if (__name__ == "__main__"):
